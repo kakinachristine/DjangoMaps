67cdeca57fde98ded94de3449d4b1ee297b1aef2
kristinekakina <kristinekakina@gmail.com>
2023-08-25 13:34:35 +0300

    app take 2

--- djangoMaps/urls.py
+++ djangoMaps/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Root URL pattern
-    path('store_geolocation/', views.store_geolocation, name='store_geolocation'),
+    # path('store_geolocation/', views.store_geolocation, name='store_geolocation'),
+    path('mongo/', views.mongo_view, name='mongo-view'),
 ]
--- djangoMaps/views.py
+++ djangoMaps/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from pymongo import MongoClient
+
 import json
+
 
 @csrf_exempt
 def store_geolocation(request):
@@ -11,11 +14,40 @@ def store_geolocation(request):
         print(latitude)
         print(longitude)
 
-        # Do something with the latitude and longitude data, like storing it in your database
+        # Connect to MongoDB and store the geolocation data
+        connection_string = "mongodb+srv://your-connection-string"
+        client = MongoClient(connection_string)
+        db = client["your-database-name"]
+        collection = db["your-collection-name"]
+
+        # Store the geolocation data in the collection
+        collection.insert_one({"latitude": latitude, "longitude": longitude})
 
         return HttpResponse("Geolocation data received and processed.")
 
+
 def index(request):
-    return render(request, 'geolocation.html')
+    return render(request, 'mongo_template.html')
 
 
+def mongo_view(request):
+    # Replace with your MongoDB Atlas connection string
+    connection_string = "mongodb+srv://V:V@Cluster0-name.mongodb.net/test"
+
+    client = MongoClient(connection_string)
+    db = client["V"]
+
+    # Create a new collection named "geolocation_data"
+    collection_name = "geolocation_data"
+    db.create_collection(collection_name)
+
+    # Check if the collection exists
+    if collection_name in db.list_collection_names():
+        collection_exists = True
+    else:
+        # Create a new collection
+        db.create_collection(collection_name)
+        collection_exists = False
+
+    # Render a template and pass the collection existence status to it
+    return render(request, 'mongo_template.html', {'collection_exists': collection_exists})
